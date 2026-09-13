d9462b44e6448b82476f6345452795869478fa4b
Adiel Firqin <147130623+NathanLouis90@users.noreply.github.com>
2026-09-13 13:46:19 +0800

    Delete install directory

--- install/semantic_msgs/local/lib/python3.10/dist-packages/semantic_msgs/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from semantic_msgs.msg._semantic_detection import SemanticDetection  # noqa: F401
-from semantic_msgs.msg._semantic_detection_array import SemanticDetectionArray  # noqa: F401
--- install/semantic_msgs/share/semantic_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(semantic_msgs_IDL_FILES "msg/SemanticDetection.idl;msg/SemanticDetectionArray.idl")
-set(semantic_msgs_INTERFACE_FILES "msg/SemanticDetection.msg;msg/SemanticDetectionArray.msg")
